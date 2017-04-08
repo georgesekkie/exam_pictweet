69ae996730247a43d3308b1e2a0a8d61de3e7ba8
関谷丈二 <joji.sekiya@gmail.com>
2017-04-08 11:31:30 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root  'tweets#index'
+
+  resources :tweets, :except => [:show]
+  # get     '/tweets'           => 'tweets#index'
+  # post    '/tweets'           => 'tweets#create'
+  # get     '/tweets/new'       => 'tweets#new'
+  # delete  '/tweets/:id'       => 'tweets#destroy'
+  # get     '/tweets/:id/edit'  => 'tweets#edit'
+  # patch   '/tweets/:id'       => 'tweets#update'
+
+  resources :users, :only => [:show]
+  # get     '/users/:id'        => 'users#show'
+end
